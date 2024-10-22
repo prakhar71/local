abb08256b56ff2baa4684e1dcbefef61bd1adbf2
prakhar vashistha <prakharvashistha@prakhars-MacBook-Pro.local>
2024-10-22 11:32:51 +0530

    left-view-tree

--- src/class1.java
+++ src/class1.java
@@ -1,48 +1,56 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
+/*
+   		5
+  6      7
+       9   8
+*/
 public class class1 {
-
-    /*
-    Write an Employee class with the following properties :
-
-Id
-Name
-Age
-Gender
-Department
-Year of joining
-
-Given a List of Employees, perform the following actions using Java 8 features (Streams, Functional programming, Lambdas, Method References)
-
-Get Names of Employees who joined after 2020. -
-Find the count of male and female employees in the organization
-Output: Male = <Count of male employees> , Female = <Count of female employees>
-       3.Get the name of the highest experienced employee in the organization
-    * */
     public static void main(String args[]) {
-        Employee e1 = new Employee();e1.yearOfJoining=2017;e1.gender="F";e1.name="A";
-        Employee e2 = new Employee();e2.yearOfJoining=2027;e2.gender="M";e2.name="B";
-        Employee e3 = new Employee();e3.yearOfJoining=2021;e3.gender="F";e3.name="C";
-        List<Employee> employeeList = new Vector<>();
-        employeeList.add(e1);employeeList.add(e2);employeeList.add(e3);
+        Node n1 = new Node(5);
+        Node n2 = new Node(6);
+        Node n3 = new Node(7);
+        Node n4 = new Node(9);
+        Node n5 = new Node(8);
 
-       //List<Employee> list = employeeList.stream().filter(emp -> emp.yearOfJoining>2020).collect(Collectors.toList());
+        n3.left = n4;n3.right=n5;
+        n1.left = n2;n1.right=n3;
 
-       // long femaleCount = employeeList.stream().filter(emp -> emp.gender == "F").count();
-        Optional<Integer> highest =  employeeList.stream().map(x->x.yearOfJoining).collect(Collectors.minBy(Comparator.naturalOrder()));
-        int age = highest.get();
-        employeeList.stream().filter(e->e.age == age).collect(Collectors.toList());
+        printLeftView(n1);
+
+    }
+
+    static void printLeftView(Node root) {
+        if (root == null) return;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                Node node = q.poll();
+
+                if (i == 0) System.out.println(node.data + " ");
+
+                if (node.left != null)
+                    q.add(node.left);
+
+                if (node.right != null)
+                    q.add(node.right);
+
+            }
+        }
     }
 
 
 }
 
-class Employee {
-    int id;
-    String name;
-    int age;
-    String gender;
-    String department;
-    int yearOfJoining;
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int x) {
+        this.data = x;
+    }
 }
